e5eae7d3ec5f45b334c051162609ba07a978cc96
Andre Paraense <andre.paraense@mobwise.com.br>
2019-04-09 11:22:00 +0100

    Refactoring the Motor Codelet

--- src/main/java/codelets/system1/motor/MotionCommanderActuator.java
+++ src/main/java/codelets/system1/motor/MotionCommanderActuator.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import br.unicamp.cst.core.entities.Memory;
-import br.unicamp.cst.core.exceptions.CodeletActivationBoundsException;
 import br.unicamp.meca.system1.codelets.MotorCodelet;
 import se.bitcraze.crazyflie.lib.crazyflie.Crazyflie;
 import se.bitcraze.crazyflie.lib.positioning.MotionCommander;
@@ -19,8 +18,6 @@ import se.bitcraze.crazyflie.lib.positioning.MotionCommander;
 public class MotionCommanderActuator extends MotorCodelet {
 	
 	private MotionCommander motionCommander;
-	
-	private Memory motionCommandMemory;
 
 	public MotionCommanderActuator(String id, Crazyflie crazyflie) {
 		super(id);
@@ -34,35 +31,11 @@ public class MotionCommanderActuator extends MotorCodelet {
 	}
 
 	@Override
-	public void accessMemoryObjects() {
+	public void proc(Memory motorMemory) {
 		
-		int index=0;
-
-		if(motionCommandMemory==null)
-			motionCommandMemory = this.getInput(id, index);
-
-	}
-
-	@Override
-	public void calculateActivation() {
-		
-		try {
-
-			setActivation(0.0d);
-
-		} catch (CodeletActivationBoundsException e){		
-
-			e.printStackTrace();
-		}
-		
-	}
-
-	@Override
-	public void proc() {
-		
-		if(motionCommandMemory!=null && motionCommandMemory.getI()!=null && motionCommandMemory.getI() instanceof ArrayList){
+		if(motorMemory!=null && motorMemory.getI()!=null && motorMemory.getI() instanceof ArrayList){
 			
-			List<Float> velocitiesAxis = (ArrayList<Float>) motionCommandMemory.getI();
+			List<Float> velocitiesAxis = (ArrayList<Float>) motorMemory.getI();
 			
 			if(velocitiesAxis != null && velocitiesAxis.size() > 0 ) {	
 				try {
